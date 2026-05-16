a70eefc2cf156c6d6d4331f79a927c41c1a3ad88
kaoutarhammaoui <kaoutarhammaoui2@gmail.com>
2026-05-16 19:39:38 +0100

    comments

---
+++ services/comments.js
@@ -0,0 +1,20 @@
+import { getItem } from "./items.js";
+
+export async function getComments(ids = []) {
+  const comments = await Promise.all(
+    ids.map(async (id) => {
+      const item = await getItem(id);
+
+      if (!item || item.deleted) return null;
+
+      const kids = item.kids?.length ? await getComments(item.kids) : [];
+
+      return {
+        ...item,
+        kids,
+      };
+    }),
+  );
+
+  return comments.filter(Boolean);
+}
